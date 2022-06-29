11b8c7c50a809a4d632be8b0c99b37fff9629ac1
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-06-29 18:09:20 +0530

    Create Binary Tree to DLL .cpp

---
+++ Binary Tree to DLL .cpp
@@ -0,0 +1,9 @@
+https://practice.geeksforgeeks.org/problems/binary-tree-to-dll/1
+
+Difference between Unique key and Primary key.
+Indexing and its use in DBMS.
+Difference between Locks and Semaphore.
+Paging and Segmentation.
+Deadlock and its conditions.
+What are TCP and UDP used for?
+Why do we use Object-Oriented Programming?
