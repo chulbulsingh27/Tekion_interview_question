7ca884df74e18e3beadd26374d10cde8fde732a4
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-06-29 18:46:58 +0530

    Update 5_question.cpp

--- 5_question.cpp
+++ 5_question.cpp
@@ -10,3 +10,8 @@ https://practice.geeksforgeeks.org/problems/check-for-bst/1/
 https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
 https://practice.geeksforgeeks.org/problems/rotten-oranges2536/1/
 print LIS from a given array
+
+
+length of LCS 
+Space optimise LCS
+print all possible LCS
