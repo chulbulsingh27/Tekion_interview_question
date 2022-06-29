61db511003d45644646713bd64853133ad40aa31
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-06-29 17:10:22 +0530

    Create 5_question.cpp

---
+++ 5_question.cpp
@@ -0,0 +1,12 @@
+Find max product in the array
+String Anagram Question
+LinkedList cycle detection
+Spiral Traversal of a matrix
+Traversal question with respect to tree
+Triplet sum Question
+Right view of Binary Tree
+https://practice.geeksforgeeks.org/problems/max-sum-without-adjacents2430/1/#
+https://practice.geeksforgeeks.org/problems/check-for-bst/1/
+https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+https://practice.geeksforgeeks.org/problems/rotten-oranges2536/1/
+print LIS from a given array
